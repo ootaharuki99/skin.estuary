1b21fc5d865d805f6fe7d4422a2a60e84ca791d4
Haruki Ota <ootaharuki99@gmail.com>
2017-04-09 17:57:37 +0900

    Made texture build script for Linux

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "----------------------------------------
+Creating XBT File..."
+
+echo ".svn
+Thumbs.db
+Desktop.ini
+dsstdfx.bin
+build.bat
+build.sh
+/skin.estuary-japanese/media/
+/skin.estuary-japanese/themes/
+exclude.txt" > exclude.txt
+
+TexturePacker -dupecheck -input media -output media\Textures.xbt
+TexturePacker -dupecheck -input themes\curial -output media\curial.xbt
+
+rm exclude.txt
